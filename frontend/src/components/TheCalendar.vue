b875134615e0c04a7d3a2bacd83184cb3abb650a
rechen-werk <adrian.vinojcic@gmail.com>
2024-06-09 01:20:59 +0200

    FRONTEND: bugfix for rendering appointments properly

--- frontend/src/components/TheCalendar.vue
+++ frontend/src/components/TheCalendar.vue
@@ -286,7 +286,7 @@ export default {
             </div>
             <div class="day-body">
               <div class="day-hour" v-for="index in 24" :key="index" />
-              <CalendarEntry v-for="appointment in weekdays[idx].content" :appointment=appointment :is-own-calendar="user.email === calendarEmail"/>
+              <CalendarEntry v-for="appointment in weekdays[idx].content" :key="weekdays[idx].content" :appointment=appointment :is-own-calendar="user.email === calendarEmail"/>
             </div>
           </div>
         </div>
